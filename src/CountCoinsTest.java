823b189f70079e3142ef84af25588c28e97e0bda
HossamSinga <168807715+HossamSinga@users.noreply.github.com>
2025-01-12 23:49:51 +0100

    Update CountCoinsTest.java

--- src/CountCoinsTest.java
+++ src/CountCoinsTest.java
@@ -1,11 +1,41 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CountCoinsTest {
     @Test
-    <CoinChange>
-    void testZeroAmount() {
-        CoinChange coinChange = new CoinChange();
-        int ways = coinChange.countWays(0);
+    public void testZeroCents(){
+        CountCoins countCoins = new CountCoins();
+           int Ways = countCoins.countWays(0);
+        assertEquals(1,Ways);
+
+    }
+    @Test
+    public void testFiveCents(){
+        CountCoins countCoins = new CountCoins();
+        int Ways = countCoins.countWays(5);
+        assertEquals(2,Ways);
+
+    }
+    @Test
+    public void testTenCents() {
+        CountCoins countCoins = new CountCoins();
+        int Ways = countCoins.countWays(10);
+        System.out.println(Ways);
+        assertEquals(4, Ways);
+    }
+    @Test
+    public void testDollar(){
+        CountCoins countCoins = new CountCoins();
+        int Ways = countCoins.countWays(100);
+        System.out.println(Ways);
+        assertEquals(242,Ways);
+
+
     }
 }
+
+
+
+
 
